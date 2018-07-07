c9935f622d750931f68b00cc85d12421418b9298
igsub <igsub@hotmail.com>
2018-07-06 21:26:03 -0300

    Agrego servicios al inicio.

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,7 +6,7 @@
 
 @section('body')
 
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide" data-pause="false" data-ride="carousel" data-interval="3500">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -14,9 +14,15 @@
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img class="d-block w-100" src="images/law1.jpg" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h1>Civil</h1>
+        </div>
       </div>
       <div class="carousel-item">
         <img class="d-block w-100" src="images/law2.jpg" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h1>Comercial</h1>
+        </div>
       </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -28,5 +34,43 @@
       <span class="sr-only">Next</span>
     </a>
   </div>
+  
+<div class="container">
+  <div class="containerservicios">
+    <div class="row">
+      <div class="col-md-4 col-sm-6 unserv">
+        <h3 class="titulo-servicio">
+          Civil
+        </h3>
+        <p>Mucha sarasa para probar una propiedad de la columna ia tu sabe</p>
+      </div>
+      <div class="col-md-4 col-sm-6 unserv">
+        <h3 class="titulo-servicio">
+          Comercial
+        </h3>
+      </div>
+      <div class="col-md-4 col-sm-6 unserv">
+        <h3 class="titulo-servicio">
+          Administrativo
+        </h3>
+      </div>
+      <div class="col-md-4 col-sm-6 unserv">
+        <h3 class="titulo-servicio">
+          Familia
+        </h3>
+      </div>
+      <div class="col-md-4 col-sm-6 unserv">
+        <h3 class="titulo-servicio">
+          Laboral
+        </h3>
+      </div>
+      <div class="col-md-4 col-sm-6 unserv">
+        <h3 class="titulo-servicio">
+          Sucesiones
+        </h3>
+      </div>
 
+    </div>
+  </div>
+</div>
 @endsection
