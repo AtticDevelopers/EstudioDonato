a9b4cd5f6be1e0a5a483c19eec32c6e82d433694
igsub <igsub@hotmail.com>
2018-07-07 20:33:45 -0300

    imagenes al carousel

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,10 +6,14 @@
 
 @section('body')
 
+  <span style="display: block; height: 100px;"></span>
+
   <div id="carouselExampleIndicators" class="carousel slide" data-pause="false" data-ride="carousel" data-interval="3500">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -24,6 +28,18 @@
           <h1>Comercial</h1>
         </div>
       </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="images/law3.jpg" alt="third slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h1>Administrativo</h1>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="images/law4.jpg" alt="fourth slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h1>Laboral</h1>
+        </div>
+      </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -34,7 +50,7 @@
       <span class="sr-only">Next</span>
     </a>
   </div>
-  
+
 <div class="container">
   <div class="containerservicios">
     <div class="row">
@@ -43,6 +59,10 @@
           Civil
         </h3>
         <p>Mucha sarasa para probar una propiedad de la columna ia tu sabe</p>
+        <h6 class="text-right">
+          <img src="images/tic2.png">
+          Me interesa
+        </h6>
       </div>
       <div class="col-md-4 col-sm-6 unserv">
         <h3 class="titulo-servicio">
