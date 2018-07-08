d676f72b714a7dfbe488b99c52c36784b3c30d5f
Matias Massetti <matiasmassetti@gmail.com>
2018-07-08 16:25:16 -0300

    Pestaña Historia

---
+++ resources/views/historia.blade.php
@@ -0,0 +1,56 @@
+@extends('layout')
+
+@section('style')
+<link rel="stylesheet" type="text/css" href="css/historia-style.css">
+@endsection
+
+@section('body')
+
+  <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading -->
+      <h1 class="my-4">Estudio Donato
+        <small>Nuestra historia</small>
+      </h1>
+
+      <div class="row">
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <href="#"><img class="card-img-top" src="images/law3.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <h2>El comienzo</h2>
+              </h4>
+              <p class="card-text">La primera oficina la abrio ... en el año ... blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images/law2.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <h2>Pasaron los años</h2>
+              </h4>
+              <p class="card-text">............................................................................................................................................................................................................................................................................................................................................................</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="images/law4.jpg" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <h2>El estudio hoy</h2>
+              </h4>
+              <p class="card-text">Contar algo</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+  </endsection>
