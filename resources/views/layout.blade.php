b85120a94192a1163fc03dd1ddf356cdfcee1c83
JUAN-PC\Juan <juuan.olmedo@hotmail.com>
2018-07-08 16:20:14 -0300

    Agregue los links del layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,25 +22,25 @@
 	<!-- Navigation -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 		<div class="container">
-			<a class="navbar-brand" href="#">Start Bootstrap</a>
+			<a class="navbar-brand" href="./">Estudio Donato</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
 				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home
+					<li class="nav-item">
+						<a class="nav-link" href="./">Inicio
 							<span class="sr-only">(current)</span>
 						</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">About</a>
+						<a class="nav-link" href="./historia">Historia</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Services</a>
+						<a class="nav-link" href="./especialistas">Especialistas</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Contact</a>
+						<a class="nav-link" href="./contacto">Contacto</a>
 					</li>
 				</ul>
 			</div>
@@ -52,7 +52,8 @@
 	<!-- Footer -->
 	<footer class="py-5 bg-dark">
 		<div class="container">
-			<p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+			<p class="m-0 text-center text-white">Estudio Donato - Copyright &copy; 2018</p>
+			<p class="m-0 text-center text-white">Attic Developers</p>
 		</div>
 		<!-- /.container -->
 	</footer>
