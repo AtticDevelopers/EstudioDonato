2f434590d452a6594dde9f8089cb19b06bdff8ec
igsub <igsub@hotmail.com>
2018-07-13 22:10:27 -0300

    Nav bar ajustada segun la imagen de estudio donato. (main style css)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,7 +25,7 @@
       <!-- Navigation -->
       <nav class="navbar navbar-expand-lg fixed-top">
          <div class="container">
-            <a class="navbar-brand" href="./"><img src=images/logoPrueba7.png alt="Estudio Donato"></a>
+            <a class="navbar-brand" href="./"><img id="estudioDonatoImg" src=images/logoPrueba7.png alt="Estudio Donato"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
             </button>
