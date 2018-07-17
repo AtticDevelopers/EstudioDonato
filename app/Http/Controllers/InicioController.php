9171b7ac1b72d8c10937d225d1259142bb843143
juanfmoyano <juanfri.m@hotmail.com>
2018-07-17 15:01:28 -0300

    CSS corregidos y cambios en controllers-blades por bd

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class InicioController extends Controller
 {
     public function getInicioView(){
-    	$arregloAreas = DB::select('select nombre, descripcionCorta from areas');
+    	$arregloAreas = DB::select('select nombreArea, descripcionCorta from areas');
     	return view('inicio', ['areas' => $arregloAreas])->with(["page" => "home"]);
     }
 
